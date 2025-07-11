77b60e2334acd58326fc227a1c842edfa0557af0
dave <dave@aggrik8s.net>
2025-07-11 18:04:04 -0400

    Sync client and server talosctl versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ module "talos-proxmox-east" {
     source  = "./modules/talos_cilium"
     #version = "0.1.5"
     talos_cluster_name = "talos-east"
-    talos_version = "1.9.5"
+    talos_version = "1.10.4"
     control_nodes = {
         "cp-e-1" = "pve"
         "cp-e-2" = "pve"
@@ -38,6 +38,27 @@ module "talos-proxmox-east" {
         "wrk-e-2" = "pve"
         "wrk-e-3" = "pve"
     }
+
+    worker_extra_disks = {
+        "wrk-e-1" = [{
+                            datastore_id = "pve-zfs-01"
+                            size         = 128
+                            #file_format  = optional(string)
+                            #file_id      = optional(string)
+                      },]
+        "wrk-e-2" = [{
+                            datastore_id = "pve-zfs-01"
+                            size         = 128
+                            #file_format  = optional(string)
+                            #file_id      = optional(string)
+                      },]
+        "wrk-e-3" = [{
+                            datastore_id = "pve-zfs-01"
+                            size         = 128
+                            #file_format  = optional(string)
+                            #file_id      = optional(string)
+                      },]
+    }
 }
 
 module "talos-proxmox-west" {
@@ -46,7 +67,7 @@ module "talos-proxmox-west" {
     source  = "./modules/talos_cilium"
     #version = "0.1.5"
     talos_cluster_name = "talos-west"
-    talos_version = "1.9.5"
+    talos_version = "1.10.4"
     control_nodes = {
         "cp-w-1" = "pve"
         "cp-w-2" = "pve"
@@ -56,6 +77,26 @@ module "talos-proxmox-west" {
         "wrk-w-1" = "pve"
         "wrk-w-2" = "pve"
         "wrk-w-3" = "pve"
+    }
+    worker_extra_disks = {
+        "wrk-w-1" = [{
+                            datastore_id = "pve-zfs-01"
+                            size         = 128
+                            #file_format  = optional(string)
+                            #file_id      = optional(string)
+                      },]
+        "wrk-w-2" = [{
+                            datastore_id = "pve-zfs-01"
+                            size         = 128
+                            #file_format  = optional(string)
+                            #file_id      = optional(string)
+                      },]
+        "wrk-w-3" = [{
+                            datastore_id = "pve-zfs-01"
+                            size         = 128
+                            #file_format  = optional(string)
+                            #file_id      = optional(string)
+                      },]
     }
 }
 
