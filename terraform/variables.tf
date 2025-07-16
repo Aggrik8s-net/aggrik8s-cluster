bf11831d445e90f46c0c3f05a9f5b4e6079b1ab4
david.shea <des9299@gmail.com>
2025-07-15 22:48:04 -0400

    Lookup Proxmox creds via Doppler provider (except var.docker_token).

---
+++ terraform/variables.tf
@@ -0,0 +1,30 @@
+# Define a variable so we can pass in our token
+variable "doppler_token" {
+  type = string
+  description = "A token to authenticate with Doppler"
+}
+
+variable "proxmox_api_token" {
+  description = "Doppler managed secret injected as a TF_VAR."
+  type = string
+  sensitive = true
+}
+
+variable "proxmox_api_endpoint" {
+  description = "Doppler managed secret injected as a TF_VAR."
+  type = string
+  sensitive = false
+  default = "https://192.168.10.10:8006"
+}
+
+variable "proxmox_root_pwd" {
+  description = "Doppler managed secret injected as a TF_VAR."
+  type = string
+  sensitive = true
+}
+
+variable "proxmox_user" {
+  description = "Doppler managed secret injected as a TF_VAR."
+  type = string
+  sensitive = false
+}
