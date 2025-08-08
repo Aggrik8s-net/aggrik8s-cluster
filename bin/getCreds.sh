29131e8c40015edf29647f342cb513ddab75fde4
dshea <dies9299@gmail.com>
2025-08-08 15:31:17 -0400

    Fix config file permissions.

--- bin/getCreds.sh
+++ bin/getCreds.sh
@@ -7,9 +7,14 @@
 # set -x
 
 terraform output --raw talosconfig-east > tmp/talosconfig-east
+chmod 600 tmp/talosconfig-east
 terraform output --raw kubeconfig-east  > tmp/kubeconfig-east
+chmod 600 tmp/kubeconfig-east
 
 terraform output --raw talosconfig-west > tmp/talosconfig-west
+chmod 600 tmp/talosconfig-west
 terraform output --raw kubeconfig-west  > tmp/kubeconfig-west
+chmod 600 tmp/kubeconfig-west
 
 KUBECONFIG=tmp/kubeconfig-east:tmp/kubeconfig-west kubectl config view --flatten > tmp/kubeconfig
+chmod 600 $KUBECONFIG
